c3a3407b8f6f1463b86c2a7767427d1e02552dbb
lvzhen <--get-all>
2023-01-19 18:29:06 +0800

    set account

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import { parse } from "yaml";
-import fs from "fs";
-import { IConfig, IAccount } from "./interface";
+import * as dotenv from 'dotenv'
+dotenv.config()
+import { parse } from 'yaml'
+import fs from 'fs'
+import { IConfig, IAccount } from './interface'
 // If config file exist read config file. else read config from environment variables.
-let configFile: any = {};
-if (fs.existsSync("./config.yaml")) {
-  const file = fs.readFileSync("./config.yaml", "utf8");
-  configFile = parse(file);
+let configFile: any = {}
+if (fs.existsSync('./config.yaml')) {
+  const file = fs.readFileSync('./config.yaml', 'utf8')
+  configFile = parse(file)
 } else {
   configFile = {
     chatGPTAccountPool: [
       {
-        email: process.env.CHAT_GPT_EMAIL,
-        password: process.env.CHAT_GPT_PASSWORD,
+        email: process.env.CHAT_GPT_EMAIL || '350241636@qq.com',
+        password: process.env.CHAT_GPT_PASSWORD || 'ak47jllk',
       },
     ],
     chatGptRetryTimes: Number(process.env.CHAT_GPT_RETRY_TIMES),
@@ -21,9 +21,9 @@ if (fs.existsSync("./config.yaml")) {
     openAIProxy: process.env.OPENAI_PROXY,
     clearanceToken: process.env.CF_CLEARANCE,
     userAgent: process.env.USER_AGENT,
-  };
+  }
 }
-dotenv.config();
+dotenv.config()
 
 export const config: IConfig = {
   chatGPTAccountPool: configFile.chatGPTAccountPool as Array<IAccount>,
@@ -33,12 +33,12 @@ export const config: IConfig = {
     // Try compatible with previous designs
     (configFile?.botConfig as Array<Map<string, string>>)?.reduce(
       (prev: string, curr: Map<string, string>) =>
-        curr.get("trigger_keywords") || "",
-      ""
+        curr.get('trigger_keywords') || '',
+      ''
     ) ||
-    "",
+    '',
   // Support openai-js use this proxy
   openAIProxy: configFile.openAIProxy,
   clearanceToken: configFile.clearanceToken,
   userAgent: configFile.userAgent,
-};
+}
